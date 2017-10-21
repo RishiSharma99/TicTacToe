4e0bd1566825e680c36d3f523311b615b75d1516
RishiSharma99 <32777328+RishiSharma99@users.noreply.github.com>
2017-10-21 23:32:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,91 +1,116 @@
-WHITE  = (255,255,255)
-BLACK  = (0,0,0)
-BLUE   = (0,128,255)
-ORANGE = (255,100,0)
-block_size=200
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED   = (230,0,0)
+BLUE  = (0,0,224)
+turn  = 0
+block_size = 120
 n = 3
-import pygame,math
-from copy import deepcopy as copy
-BOARD = [[0]*n]*n
 
-def win():
-	global BOARD
-	for i in range(n):
-		if BOARD[i][0]==BOARD[i][1]==BOARD[i][2]!=0:
-			return True
-		if BOARD[0][i]==BOARD[1][i]==BOARD[2][i]!=0:
-			return True
-	if BOARD[0][0]==BOARD[1][1]==BOARD[2][2]!=0:
-		return True
-	if BOARD[0][1]==BOARD[1][1]==BOARD[2][1]!=0:
-		return True
-	return False
-
-def draw_Board():
-	global BOARD , n , block_size , screen , BLACK , BLUE , ORANGE
-	for x in range(n):
-			for y in range(n):
-				if BOARD[y][x]==0:
-					rect=pygame.Rect(x*block_size,y*block_size,block_size,block_size)
-					pygame.draw.rect(screen,BLACK,rect,5)
-				elif BOARD[y][x]==1:
-					rect=pygame.Rect(x*block_size,y*block_size,block_size,block_size)
-					pygame.draw.rect(screen,BLUE,rect)
-					pygame.draw.rect(screen,BLACK,rect,5)
-				elif BOARD[y][x]==2:
-					rect=pygame.Rect(x*block_size,y*block_size,block_size,block_size)
-					pygame.draw.rect(screen,ORANGE,rect)
-					pygame.draw.rect(screen,BLACK,rect,5)
-
+import pygame
 pygame.init()
-screen = pygame.display.set_mode((n*block_size,n*block_size))
+
+class Tile():
+	def __init__(self):
+		global block_size
+		self.tile = pygame.Surface((block_size , block_size))
+
+	def draw(self , row , col , value,screen):
+		self.tile.fill(WHITE)
+		global block_size , BLACK , BLUE , RED
+		if value == 0:
+			rect=pygame.Rect(0,0,block_size,block_size)
+			pygame.draw.rect(self.tile , BLACK , rect ,5)
+		if value == 1:
+			pygame.draw.circle(self.tile , BLUE , (block_size//2 , block_size//2) , block_size//2 , 5)
+			rect=pygame.Rect(0,0,block_size,block_size)
+			pygame.draw.rect(self.tile , BLACK , rect ,5)
+		if value == -1:
+			pygame.draw.line(self.tile , RED , (0,0) , (block_size , block_size) , 5)
+			pygame.draw.line(self.tile , RED , (block_size,0) , (0 , block_size) , 5)
+			rect=pygame.Rect(0,0,block_size,block_size)
+			pygame.draw.rect(self.tile , BLACK , rect ,5)
+		screen.blit(self.tile , (row*block_size , col*block_size))
+		pygame.display.flip()
+
+
+
+
+class Board():
+	def __init__(self):
+		global n
+		self.draw_board = [[0 for x in range(n)] for y in range(n)]
+		self.win_board  = [0 for _ in range(2*n+2)]
+
+	def test(self):
+		print(self.win_board)
+
+	def player_move(self , row , col):
+		global turn , n
+		a = 0
+		if (turn%2==0):
+			a = 1
+		else:
+			a = -1
+		if self.draw_board[row][col]==0:
+			
+			self.draw_board[row][col] = a
+			self.win_board[row]      += a
+			self.win_board[n+col]    += a
+			
+			if (row==col):
+				self.win_board[2*n]  += a
+			
+			if (row + col == n-1):
+				self.win_board[2*n+1]+= a
+			turn+=1
+
+	def win(self):
+		global n
+		for e in self.win_board:
+			if e==n or e==-n:
+				return True
+		return False
+
+	def draw(self,screen):
+		global n
+		for row in range(n):
+			for col in range(n):
+				value = self.draw_board[row][col]
+				box = Tile()
+				box.draw(row , col , value , screen)
+
+n=int(input("No. Of tiles: "))
+screen = pygame.display.set_mode((n*block_size , n*block_size))
 screen.fill(WHITE)
+board = Board()
+
 
 done = False
 
-
-pygame.display.flip()
 clock = pygame.time.Clock()
+font = pygame.font.SysFont("comicsansms" , 52)
 
-turn=0
-x,y=0,0
-flag = False
-
-font = pygame.font.SysFont("comicsansms" , 72)
+row,col = 0,0
 
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
 		if event.type == pygame.MOUSEBUTTONDOWN:
-			x,y=pygame.mouse.get_pos()
-			x = math.floor(x/block_size)
-			y = math.floor(y/block_size)
-			if BOARD[y][x]==0 and not flag:	
-				temp=copy(BOARD[y])
-				temp[x]=turn%2+1
-				BOARD[y]=temp
-				turn+=1				
-	
-	screen.fill(WHITE)
-	if not win() and turn != n**2:
-		draw_Board()
-	elif win():
-		flag=True
-		draw_Board()
-		s=pygame.Surface((n*block_size,n*block_size))
-		s.set_alpha(128)
-		s.fill(WHITE)
-		text = font.render("PLAYER-"+str((turn-1)%2+1)+" WINS!!!!" , True , ORANGE)
-		s.blit(text , (n*block_size/2-text.get_width()//2 , n*block_size/2-text.get_height()//2))
-		screen.blit(s,(0,0))
-	elif not win() and turn == n**2:
-		draw_Board()
-		s=pygame.Surface((n*block_size,n*block_size))
-		s.set_alpha(128)
-		s.fill(WHITE)
-		match_draw = font.render("DRAW!!!", True , BLACK)
-		s.blit(match_draw , (n*block_size/2-match_draw.get_width()//2 , n*block_size/2-match_draw.get_height()//2))
-		screen.blit(s,(0,0))
-	pygame.display.flip()
+			x,y = pygame.mouse.get_pos()
+			row = x//block_size
+			col = y//block_size
+			board.player_move(row , col)
+	if not board.win():
+		board.draw(screen)
+	else:
+		screen.fill(WHITE)
+		if turn != n-1:
+			text = font.render("PLAYER-"+str((turn-1)%2+1)+" WINS!!!!" , True , BLACK)
+			screen.blit(text , (n*block_size/2-text.get_width()//2 , n*block_size/2-text.get_height()//2))
+		else:
+			text = font.render("DRAW!!!" , True , BLACK)
+			screen.blit(text , (n*block_size/2-text.get_width()//2 , n*block_size/2-text.get_height()//2))
+		pygame.display.flip()
+
 	clock.tick(60)
